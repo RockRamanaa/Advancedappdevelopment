044b51501efcb1e5cbc43b1c64dbfd475d91aafc
RockRamanaa <145009921+RockRamanaa@users.noreply.github.com>
2024-03-25 16:09:38 +0530

    Add files via upload

---
+++ Day-10/src/test/java/com/chessacademy/chessadmission/ChessadmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chessacademy.chessadmission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessadmissionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
