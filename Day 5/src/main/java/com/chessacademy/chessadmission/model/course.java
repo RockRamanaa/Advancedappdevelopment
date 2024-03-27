0a0ad9cecdef8ddee9673ed9757636ed66827f7f
RockRamanaa <145009921+RockRamanaa@users.noreply.github.com>
2024-03-27 10:34:37 +0530

    Add files via upload

---
+++ Day 5/src/main/java/com/chessacademy/chessadmission/model/course.java
@@ -0,0 +1,9 @@
+package com.chessacademy.chessadmission.model;
+
+public class course {
+    
+     private String course;
+     private int timings;
+     Private String level;
+     
+}
---
+++ Day 5/src/main/java/com/chessacademy/chessadmission/model/user.java
@@ -0,0 +1,9 @@
+package com.chessacademy.chessadmission.model;
+
+public class user {
+    private String firstname;
+    private String lastname;
+    private string email;
+    private string password;
+    
+}
